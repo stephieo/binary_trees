d377b05b7130142c799114bab6b8bb17cfbef5f7
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-01-31 16:21:56 +0100

    feat: solution to tasks 1 and 0

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - inserts a node as the left child of another node
+ * @parent: pointer to the node to insert the left child
+ * @value: value to store in new node
+ *
+ * Description: inserts a left child to a node. if left child
+ * is present it becomes left child of the incoming node
+ *
+ * Return: pointer to created node or NULL if null parent or on failure
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	/*create node*/
+	binary_tree_t *newnode;
+
+	newnode = binary_tree_node(parent, value);
+
+	newnode->parent = parent;
+	if (parent->left)
+	{
+		parent->left->parent = newnode;
+		parent->left  = newnode;
+
+	}
+	else
+	{
+		parent->left = newnode;
+	}
+
+	return (newnode);
+}
+
